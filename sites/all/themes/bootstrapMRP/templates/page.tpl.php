2e07efae32bba7f2692dccaaaf2f2634bdb65a0a
Joe Caruso <joseph.caruso@nau.edu>
2013-06-12 14:37:57 -0700

    bootstrap sub-theme, with custom responisve nau header

---
+++ sites/all/themes/bootstrapMRP/templates/page.tpl.php
@@ -0,0 +1,101 @@
+<header id="navbar" role="banner" class="navbar navbar-fixed-top">
+
+	<!-- title bar -->
+	<section id="app-title-bar">
+		<div class="nau-logo-title">
+			<div id="app-logo" class="pull-left">
+				<img src="/sites/all/themes/bootstrapMRP/images/logo-alt.png" alt="Northern Arizona University" title="Northern Arizona University"/>
+			</div>
+			<div id="app-title" style="padding:4px;">
+     				Mountian Research Productions - <?php print $site_slogan; ?>
+			</div>
+		</div>
+  </section>
+  <!-- end of title bar -->
+
+  <div class="navbar-inner">
+    <div class="container">
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+
+
+      <?php if (!empty($site_name)): ?>
+        <h1 id="site-name">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
+        </h1>
+      <?php endif; ?>
+
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+        <div class="nav-collapse collapse">
+          <nav role="navigation">
+            <?php if (!empty($primary_nav)): ?>
+              <?php print render($primary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($secondary_nav)): ?>
+              <?php print render($secondary_nav); ?>
+            <?php endif; ?>
+            <?php if (!empty($page['navigation'])): ?>
+              <?php print render($page['navigation']); ?>
+            <?php endif; ?>
+          </nav>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
+</header>
+
+<div class="main-container container">
+
+  <header role="banner" id="page-header">
+
+
+    <?php print render($page['header']); ?>
+  </header> <!-- /#header -->
+
+  <div class="row-fluid">
+
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside class="span3" role="complementary">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>  <!-- /#sidebar-first -->
+    <?php endif; ?>
+
+    <section class="<?php print _bootstrap_content_span($columns); ?>">
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if (!empty($title)): ?>
+        <h1 class="page-header"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if (!empty($page['help'])): ?>
+        <div class="well"><?php print render($page['help']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+    </section>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside class="span3" role="complementary">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>  <!-- /#sidebar-second -->
+    <?php endif; ?>
+
+  </div>
+</div>
+<footer class="footer container">
+  <?php print render($page['footer']); ?>
+</footer>
